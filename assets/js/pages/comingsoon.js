e77f1c8feb7fa2a70eb26b34a6d0e921eac5f63d
Ahmet Emin ŞİT <sitahmetemin@hotmail.com>
2019-11-11 10:58:36 +0300

    comingsoon sayfasına timer eklendi

---
+++ assets/js/pages/comingsoon.js
@@ -0,0 +1,39 @@
+timerSet = (showDate) => {
+    // Set the date we're counting down to
+    var countDownDate = new Date(showDate).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("timer").innerHTML =
+            '<div class="days">' +
+            days +
+            'gün </div> <div class="hours">' +
+            hours +
+            'saat </div> <div class="minutes"> ' +
+            minutes +
+            'dakida </div> <div class="seconds">' +
+            seconds +
+            "saniye </div>";
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = '<div class="exp">Geçti</div>';
+        }
+    }, 1000);
+};
